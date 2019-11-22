eaafec29ccfab776cfa6af3ac316173dc86a9860
Ricardo Linares <richiexlinares@gmail.com>
2019-11-22 14:07:12 +0100

    My commit

---
+++ guest_list.py
@@ -0,0 +1,9 @@
+#I'm inviting some friends for dinner
+guest_list = ['beto', 'rob', 'jose']
+
+#rob won't make it so I invited sergio instead
+guest_list.remove('rob')
+guest_list.insert(1, 'sergio')
+print(guest_list)
+
+
