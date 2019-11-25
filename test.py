d2460f91f1b4d4dcdaf584a8db96ff375e235377
Ricardo Linares <ricardo.linares@protonmail.com>
2019-11-25 11:30:53 +0100

    update

---
+++ test.py
@@ -0,0 +1,8 @@
+pizzas = ['hawaian', 'kebab', 'napolitana']
+for pizza in pizzas:
+    print(f"I like {pizza.title()} pizza!")
+print("I really love pizza!")
+animals = ['dog', 'cat', 'hamster']
+for animal in animals:
+    print(f"A {animal.title()} would make a great pet!")
+print("Any of these animals would make a great pet!")
