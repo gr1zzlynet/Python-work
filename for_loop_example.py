d51c21e2c1c1a7168ae34de0304a65343982a3d7
Ricardo Linares <ricardo.linares@protonmail.com>
2019-11-25 11:50:54 +0100

    update

--- for_loop_example.py
+++
@@ -1,4 +0,0 @@
-pizzas = ['hawaian', 'kebab', 'napolitana']
-for pizza in pizzas:
-    print(f"I like {pizza.title()} pizza!")
-print("I really love pizza!")
--- guest_list.py
+++
@@ -1,9 +0,0 @@
-#I'm inviting some friends for dinner
-guest_list = ['beto', 'rob', 'jose']
-
-#rob won't make it so I invited sergio instead
-guest_list.remove('rob')
-guest_list.insert(1, 'sergio')
-print(guest_list)
-
-
